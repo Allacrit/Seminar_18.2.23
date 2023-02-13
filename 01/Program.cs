11d85c8d01403969165e8941ce9e56c7e26e7e6f
Artem Oleynikov <allacrit@yandex.ru>
2023-02-13 22:46:19 +0300

    Создание решения задачи №1

---
+++ 01/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("1. Показать натуральные числа от 1 до N, N задано.");
+Console.WriteLine(NumbersRange1NRec(10));
+
+string NumbersRange1NRec(int N)
+{
+    return N < 1 ? String.Empty : NumbersRange1NRec(N - 1) + $"{N} ";
+}

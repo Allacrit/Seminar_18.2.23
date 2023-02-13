368753a14ce0d83065c4d5f48ad03cf2db9fa5cc
Artem Oleynikov <allacrit@yandex.ru>
2023-02-13 22:51:07 +0300

    Создание решения задачи №3

---
+++ 03/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("3. Показать натуральные числа от M до N, N и M заданы.");
+Console.WriteLine(NumbersRangeMN(10, 20));
+
+string NumbersRangeMN(int M, int N)
+{
+    return M > N ? String.Empty : $"{M} " + NumbersRangeMN(M + 1, N);
+}

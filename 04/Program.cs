6e39628db55140b2ca3e0bd304eb2752d1d5c946
Artem Oleynikov <allacrit@yandex.ru>
2023-02-13 22:53:05 +0300

    Создание решения задачи №4

---
+++ 04/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("4. Найти сумму элементов от M до N, N и M заданы.");
+Console.WriteLine(SumRangeMN(3, 7));
+
+int SumRangeMN(int M, int N)
+{
+    return M > N ? 0 : M + SumRangeMN(M + 1, N);
+}

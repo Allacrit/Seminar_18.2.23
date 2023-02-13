79b8e6ee72d709cfd2c9bf351bd4bdb55c8146fa
Artem Oleynikov <allacrit@yandex.ru>
2023-02-13 23:00:26 +0300

    Создание решения задачи №5

---
+++ 05/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("5. Найти сумму цифр числа: ");
+
+int number = int.Parse(Console.ReadLine());
+
+Console.WriteLine(SplitNumber(number));
+
+int SplitNumber(int number)
+{
+    if (number == 0) return 0;
+    else return number % 10 + SplitNumber(number / 10);
+}
